67502d3ced496ff601298564bfe09e1317bb827c
Shivani Pandey <104144826+shivanipandey22@users.noreply.github.com>
2022-05-09 14:48:00 +0530

    Update womenPage.js

--- womenPage.js
+++ womenPage.js
@@ -3,24 +3,24 @@ var data = [
         image: "https://cdn.shopify.com/s/files/1/0248/3473/6191/products/IRtvNkoig8EGOV5nWFXpHIn8ITA17akU-25_large.jpg?v=1651300682",
         brand: "BCBGMAXAZRIA",
         name: "WOMENS SATIN SMOCKED WAIST EVENING DRESS",
-        offer_price: "130.99",
-        original_price: "145.99",
+        original_price: "130.99",
+        offer_price: "145.99",
         color:"Multi"
     },
     {
         image: "https://cdn.shopify.com/s/files/1/0248/3473/6191/products/kmeRDBDg4kSFeUkS840inVvZZtYYBXqU-25_large.jpg?v=1651399591",
         brand: "HUTCH",
         name: "MINDY WOMENS FLORAL CALF MIDI DRESS",
-        offer_price: "83.99",
-        original_price: "135.99",
+        original_price: "83.99",
+        offer_price: "135.99",
         color:"Multi"
     },
     {
         image: "https://cdn.shopify.com/s/files/1/0248/3473/6191/products/XYzPFdx5coB3dMD6jzW7ir30RYyUWTH6-25_large.jpg?v=1650539762",
         brand: "JS COLLECTIONS",
         name: "WOMENS LACE SOUTACHE EVENING DRESS",
-        offer_price: "33.99",
-        original_price: "65.99",
+        original_price: "33.99",
+        offer_price: "65.99",
         color:"Multi"
 
     },
@@ -28,8 +28,8 @@ var data = [
         image: " https://cdn.shopify.com/s/files/1/0248/3473/6191/products/FM6CsxWoerO0tSOM0VKV0FzuNAxVgtOC-25_large.jpg?v=1650539763",
         brand: "KAY UNGER NEW YORK",
         name: "CLAUDIA WOMENS EMBROIDERED MAXI EVENING DRESS",
-        offer_price: "168.99",
-        original_price: "199.99",
+        original_price: "168.99",
+        offer_price: "199.99",
         color:"Multi"
 
     },
@@ -37,8 +37,8 @@ var data = [
         image: " https://cdn.shopify.com/s/files/1/0248/3473/6191/products/XmtElrvBsveYwkYKbaWQcRqq0NM2fKXl-25_d9e6cd23-fe66-428c-b10c-3417fde1f913_large.jpg?v=1651307960",
         brand: "BCBGMAXAZRIA",
         name: "BCBG MAX AZRIA WOMENS SNAKE PRINT HI-LOW WRAP DRESS",
-        offer_price: "258",
-        original_price: "366.99",
+        original_price: "258",
+        offer_price: "366.99",
         color:"Multi"
 
     },
@@ -46,8 +46,8 @@ var data = [
         image: "https://cdn.shopify.com/s/files/1/0248/3473/6191/products/xYos1PCs8nB4LjpnVx09esKf8rfAHlRa-25_large.jpg?v=1651307961",
         brand: "HALSTON",
         name: "HALSTON ADELINA JERSEY DRESS",
-        offer_price: "395",
-        original_price: "659.99",
+        original_price: "395",
+        offer_price: "659.99",
         color:"Multi"
 
     },
@@ -55,8 +55,8 @@ var data = [
         image: "https://cdn.shopify.com/s/files/1/0248/3473/6191/products/HCKLJco07lMnkciozH7LiP02eGWkejan-25_large.jpg?v=1650917439",
         brand: "KENDALL + KYLIE",
         name: "WOMENS REVERSIBLE WINTER FAUX FUR COAT",
-        offer_price: "45.99",
-        original_price: "60.99",
+        original_price: "45.99",
+        offer_price: "60.99",
         color:"Multi"
 
     },
@@ -64,8 +64,8 @@ var data = [
         image: "https://cdn.shopify.com/s/files/1/0248/3473/6191/products/WtIkC95Sk1Q8BQVtDO7za1VWhDs6n8Pt-25_large.jpg?v=1650917439",
         brand: "FLOR ET.AL",
         name: "AUGUSTUS WOMENS CASCADE RUFFLE PROM EVENING DRESS",
-        offer_price: "209.99",
-        original_price: "279.99",
+        original_price: "209.99",
+        offer_price: "279.99",
         color:"Multi"
 
     },
@@ -73,8 +73,8 @@ var data = [
         image: "https://cdn.shopify.com/s/files/1/0248/3473/6191/products/1050230427_RLLZ_1_large.jpg?v=1651397252",
         brand: "TAHARI",
         name: "MARILYN WOMENS WOOL BLEND WINTER WRAP COAT",
-        offer_price: "77.99",
-        original_price: "126.99",
+        original_price: "77.99",
+        offer_price: "126.99",
         color:"Multi"
 
     },
@@ -82,8 +82,8 @@ var data = [
         image: "https://cdn.shopify.com/s/files/1/0248/3473/6191/products/1050230427_RLLZ_2_large.jpg?v=1651397253",
         brand: "BOTTEGA VENETA",
         name: "BUBBLE GUM WASHED JACKET",
-        offer_price: "1514",
-        original_price: "1962.47",
+        original_price: "1514",
+        offer_price: "1962.47",
         color:"Multi"
 
     },
@@ -91,8 +91,8 @@ var data = [
         image: "https://cdn.shopify.com/s/files/1/0248/3473/6191/products/RQPfw15AQ9GP5g92OQkaKL60129tNY6I-25_large.jpg?v=1651511039",
         brand: "JVN BY JOVANI",
         name: "WOMENS METALLIC MERMAID EVENING DRESS",
-        offer_price: "71.99",
-        original_price: "113.99",
+        original_price: "71.99",
+        offer_price: "113.99",
         color:"Multi"
 
     },
@@ -100,8 +100,8 @@ var data = [
         image: "https://cdn.shopify.com/s/files/1/0248/3473/6191/products/tE9p7IJ8j7kTb7v66qH5x5Vxg4QoRYF0-25_large.jpg?v=1651511040",
         brand: "ALESSANDRA RICH",
         name: "POLKA DOT SILK DRESS",
-        offer_price: "1962",
-        original_price: "2984.15",
+        original_price: "1962",
+        offer_price: "2984.15",
         color:"Multi"
 
     },
@@ -109,8 +109,8 @@ var data = [
         image: " https://cdn.shopify.com/s/files/1/0248/3473/6191/products/IPvvoiIis0dIOS3j36PV616zIxnTp937-25_large.jpg?v=1651306193",
         brand: "AIDAN MATTOX",
         name: "WOMENS OFF-THE-SHOULDER RUFFLED EVENING DRESS",
-        offer_price: "40.99",
-        original_price: "61.99",
+        original_price: "40.99",
+        offer_price: "61.99",
         color:"Multi"
 
     },
@@ -118,8 +118,8 @@ var data = [
         image: "https://cdn.shopify.com/s/files/1/0248/3473/6191/products/Oq7wU3gxU6siVBwhHV7LddDVNQy1Qk0E-25_large.jpg?v=1651306194",
         brand: "BALENCIAGA",
         name: "ALL-OVER LOGO JACKET",
-        offer_price: "1641",
-        original_price: "1878.03",
+        original_price: "1641",
+        offer_price: "1878.03",
         color:"Multi"
 
     },
@@ -127,8 +127,8 @@ var data = [
         image: "https://cdn.shopify.com/s/files/1/0248/3473/6191/products/zI5geEmXxlbVZnV01piDKZTRYzPo7aC4-25_22d2db44-1f3f-4135-ab5e-a41d0c214f71_large.jpg?v=1651569460",
         brand: "ML MONIQUE LHUILLIER",
         name: "WOMENS PRINTED SHUTTER PLEAT MIDI DRESS",
-        offer_price: "125.99",
-        original_price: "181.99",
+        original_price: "125.99",
+        offer_price: "181.99",
         color:"Multi"
 
     },
@@ -136,8 +136,8 @@ var data = [
         image: "https://cdn.shopify.com/s/files/1/0248/3473/6191/products/FDGlUkwu6ZR4N9sfHFc3JzldT0vfgNOY-25_large.jpg?v=1651569462",
         brand: "HALSTON",
         name: "HALSTON REMI JERSY DRESS",
-        offer_price: "445",
-        original_price: "599.99",
+        original_price: "445",
+        offer_price: "599.99",
         color:"Multi"
 
     },
@@ -145,8 +145,8 @@ var data = [
         image: "https://cdn.shopify.com/s/files/1/0248/3473/6191/products/2210583_large.jpg?v=1648529179",
         brand: "BURRYCO",
         name: "BURRYCO DRESS",
-        offer_price: "545",
-        original_price: "639.99",
+        original_price: "545",
+        offer_price: "639.99",
         color:"Multi"
 
     },
@@ -154,8 +154,8 @@ var data = [
         image: "https://cdn.shopify.com/s/files/1/0248/3473/6191/products/smyXFugo1Xekkh6WUjigTWc03QwVmCDR-25_8e4fdd80-2638-4c9f-96ba-5cbe857648e3_large.jpg?v=1651387778",
         brand: "MACKAGE",
         name: "MACKAGE CHARA X LEATHER-TRIM DOWN PARKA",
-        offer_price: "1590",
-        original_price: "1808.99",
+        original_price: "1590",
+        offer_price: "1808.99",
         color:"Multi"
 
     },
@@ -163,8 +163,8 @@ var data = [
         image: "https://cdn.shopify.com/s/files/1/0248/3473/6191/products/UelAoiRU985NiGQolthrQRMAlCY6Gv2b-25_652430bf-e39f-4fa3-a4b8-2cfcdb59357f_large.jpg?v=1651387779",
         brand: "BCBGMAXAZRIA",
         name: "BCBG MAX AZRIA WOMENS EVE POLKA DOT RUFFLED BODYCON DRESS",
-        offer_price: "63.99",
-        original_price: "179.99",
+        original_price: "63.99",
+        offer_price: "179.99",
         color:"Multi"
 
     },
@@ -172,16 +172,16 @@ var data = [
         image: "https://cdn.shopify.com/s/files/1/0248/3473/6191/products/kmeRDBDg4kSFeUkS840inVvZZtYYBXqU-25_large.jpg?v=1651399591",
         brand: "HUTCH",
         name: "MINDY WOMENS FLORAL CALF MIDI DRESS",
-        offer_price: "83.99",
-        original_price: "145.99",
+        original_price: "83.99",
+        offer_price: "145.99",
         color:"Multi"
     },
     {
         image: "https://cdn.shopify.com/s/files/1/0248/3473/6191/products/XYzPFdx5coB3dMD6jzW7ir30RYyUWTH6-25_large.jpg?v=1650539762",
         brand: "JS COLLECTIONS",
         name: "WOMENS LACE SOUTACHE EVENING DRESS",
-        offer_price: "33.99",
-        original_price: "65.99",
+        original_price: "33.99",
+        offer_price: "65.99",
         color:"Multi"
 
     },
@@ -189,8 +189,8 @@ var data = [
         image: " https://cdn.shopify.com/s/files/1/0248/3473/6191/products/FM6CsxWoerO0tSOM0VKV0FzuNAxVgtOC-25_large.jpg?v=1650539763",
         brand: "KAY UNGER NEW YORK",
         name: "CLAUDIA WOMENS EMBROIDERED MAXI EVENING DRESS",
-        offer_price: "168.99",
-        original_price: "269.99",
+        original_price: "168.99",
+        offer_price: "269.99",
         color:"Multi"
 
     },
@@ -198,8 +198,8 @@ var data = [
         image: " https://cdn.shopify.com/s/files/1/0248/3473/6191/products/XmtElrvBsveYwkYKbaWQcRqq0NM2fKXl-25_d9e6cd23-fe66-428c-b10c-3417fde1f913_large.jpg?v=1651307960",
         brand: "BCBGMAXAZRIA",
         name: "BCBG MAX AZRIA WOMENS SNAKE PRINT HI-LOW WRAP DRESS",
-        offer_price: "258",
-        original_price: "366.99",
+        original_price: "258",
+        offer_price: "366.99",
         color:"Multi"
 
     },
@@ -207,8 +207,8 @@ var data = [
         image: "https://cdn.shopify.com/s/files/1/0248/3473/6191/products/xYos1PCs8nB4LjpnVx09esKf8rfAHlRa-25_large.jpg?v=1651307961",
         brand: "HALSTON",
         name: "HALSTON ADELINA JERSEY DRESS",
-        offer_price: "395",
-        original_price: "559.99",
+        original_price: "395",
+        offer_price: "559.99",
         color:"Multi"
 
     },
@@ -216,8 +216,8 @@ var data = [
         image: "https://cdn.shopify.com/s/files/1/0248/3473/6191/products/HCKLJco07lMnkciozH7LiP02eGWkejan-25_large.jpg?v=1650917439",
         brand: "KENDALL + KYLIE",
         name: "WOMENS REVERSIBLE WINTER FAUX FUR COAT",
-        offer_price: "45.99",
-        original_price: "60.99",
+        original_price: "45.99",
+        offer_price: "60.99",
         color:"Multi"
 
     },
@@ -225,8 +225,8 @@ var data = [
         image: "https://cdn.shopify.com/s/files/1/0248/3473/6191/products/WtIkC95Sk1Q8BQVtDO7za1VWhDs6n8Pt-25_large.jpg?v=1650917439",
         brand: "FLOR ET.AL",
         name: "AUGUSTUS WOMENS CASCADE RUFFLE PROM EVENING DRESS",
-        offer_price: "209.99",
-        original_price: "229.99",
+        original_price: "209.99",
+        offer_price: "229.99",
         color:"Multi"
 
     },
@@ -234,8 +234,8 @@ var data = [
         image: "https://cdn.shopify.com/s/files/1/0248/3473/6191/products/1050230427_RLLZ_1_large.jpg?v=1651397252",
         brand: "TAHARI",
         name: "MARILYN WOMENS WOOL BLEND WINTER WRAP COAT",
-        offer_price: "77.99",
-        original_price: "126.99",
+        original_price: "77.99",
+        offer_price: "126.99",
         color:"Multi"
 
     },
@@ -243,8 +243,8 @@ var data = [
         image: "https://cdn.shopify.com/s/files/1/0248/3473/6191/products/1050230427_RLLZ_2_large.jpg?v=1651397253",
         brand: "BOTTEGA VENETA",
         name: "BUBBLE GUM WASHED JACKET",
-        offer_price: "1514",
-        original_price: "2162.47",
+        original_price: "1514",
+        offer_price: "2162.47",
         color:"Multi"
 
     },
@@ -252,8 +252,8 @@ var data = [
         image: "https://cdn.shopify.com/s/files/1/0248/3473/6191/products/RQPfw15AQ9GP5g92OQkaKL60129tNY6I-25_large.jpg?v=1651511039",
         brand: "JVN BY JOVANI",
         name: "WOMENS METALLIC MERMAID EVENING DRESS",
-        offer_price: "71.99",
-        original_price: "113.99",
+        original_price: "71.99",
+        offer_price: "113.99",
         color:"Multi"
 
 
@@ -262,8 +262,8 @@ var data = [
         image: "https://cdn.shopify.com/s/files/1/0248/3473/6191/products/tE9p7IJ8j7kTb7v66qH5x5Vxg4QoRYF0-25_large.jpg?v=1651511040",
         brand: "ALESSANDRA RICH",
         name: "POLKA DOT SILK DRESS",
-        offer_price: "1962",
-        original_price: "2184.15",
+        original_price: "1962",
+        offer_price: "2184.15",
         color:"Multi"
 
     },
@@ -271,8 +271,8 @@ var data = [
         image: " https://cdn.shopify.com/s/files/1/0248/3473/6191/products/IPvvoiIis0dIOS3j36PV616zIxnTp937-25_large.jpg?v=1651306193",
         brand: "AIDAN MATTOX",
         name: "WOMENS OFF-THE-SHOULDER RUFFLED EVENING DRESS",
-        offer_price: "40.99",
-        original_price: "61.99",
+        original_price: "40.99",
+        offer_price: "61.99",
         color:"Multi"
 
     },
@@ -280,8 +280,8 @@ var data = [
         image: "https://cdn.shopify.com/s/files/1/0248/3473/6191/products/Oq7wU3gxU6siVBwhHV7LddDVNQy1Qk0E-25_large.jpg?v=1651306194",
         brand: "BALENCIAGA",
         name: "ALL-OVER LOGO JACKET",
-        offer_price: "1641",
-        original_price: "1078.03",
+        original_price: "1641",
+        offer_price: "1078.03",
         color:"Multi"
 
     },
@@ -289,8 +289,8 @@ var data = [
         image: "https://cdn.shopify.com/s/files/1/0248/3473/6191/products/zI5geEmXxlbVZnV01piDKZTRYzPo7aC4-25_22d2db44-1f3f-4135-ab5e-a41d0c214f71_large.jpg?v=1651569460",
         brand: "ML MONIQUE LHUILLIER",
         name: "WOMENS PRINTED SHUTTER PLEAT MIDI DRESS",
-        offer_price: "125.99",
-        original_price: "181.99",
+        original_price: "125.99",
+        offer_price: "181.99",
         color:"Multi"
 
     },
@@ -298,8 +298,8 @@ var data = [
         image: "https://cdn.shopify.com/s/files/1/0248/3473/6191/products/FDGlUkwu6ZR4N9sfHFc3JzldT0vfgNOY-25_large.jpg?v=1651569462",
         brand: "HALSTON",
         name: "HALSTON REMI JERSY DRESS",
-        offer_price: "445",
-        original_price: "199.99",
+        original_price: "445",
+        offer_price: "199.99",
         color:"Multi"
 
     },
@@ -307,8 +307,8 @@ var data = [
         image: "https://cdn.shopify.com/s/files/1/0248/3473/6191/products/2210583_large.jpg?v=1648529179",
         brand: "BURRYCO",
         name: "BURRYCO DRESS",
-        offer_price: "545",
-        original_price: "139.99",
+        original_price: "545",
+        offer_price: "139.99",
         color:"Multi"
 
     },
@@ -316,8 +316,8 @@ var data = [
         image: "https://cdn.shopify.com/s/files/1/0248/3473/6191/products/smyXFugo1Xekkh6WUjigTWc03QwVmCDR-25_8e4fdd80-2638-4c9f-96ba-5cbe857648e3_large.jpg?v=1651387778",
         brand: "MACKAGE",
         name: "MACKAGE CHARA X LEATHER-TRIM DOWN PARKA",
-        offer_price: "1590",
-        original_price: "808.99",
+        original_price: "1590",
+        offer_price: "808.99",
         color:"Multi"
 
     },
